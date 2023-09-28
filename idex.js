0ce699ccbcf750b63ef5890d98ed9cb796da7990
Vijay B R <101352511+vijaybr19@users.noreply.github.com>
2023-09-28 13:40:50 +0530

    Hi

---
+++ idex.js
@@ -0,0 +1,29 @@
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+    message : "Type the url",
+    
+    name : "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qrcode.png'));
+
+    fs.writeFile("URL.txt" , url,(err) => {
+        if (err) throw err;
+        console.log("The file has been saved !");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
